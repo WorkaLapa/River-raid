a52caea9672cc513537c048babc57a779cf59da5
julfij <106023716+julfij@users.noreply.github.com>
2022-05-24 21:10:28 +0200

    Update klasy_obiektów.py

--- klasy_obiektów.py
+++ klasy_obiektów.py
@@ -1,1 +1,15 @@
+import pygame
 
+class Tankowiec:
+  def __init__(self):
+    self.x_cord = 0
+    self.y_cord = 0
+    self.image = pygame.image.load("tankowiec.png")
+    
+  def tick(self):
+    pass
+  
+  def draw(self):
+    window.blit(self.image(self.x_cord,self.y_cord)
+               
+       
