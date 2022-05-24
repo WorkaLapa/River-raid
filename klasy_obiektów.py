0e37f08c049e2c59ece867868884d03e04ef4fc0
julfij <106023716+julfij@users.noreply.github.com>
2022-05-24 21:18:29 +0200

    Update klasy_obiektów.py

--- klasy_obiektów.py
+++ klasy_obiektów.py
@@ -11,5 +11,55 @@ class Tankowiec:
   
   def draw(self):
     window.blit(self.image(self.x_cord,self.y_cord)
-               
-       
+          
+                
+class Helikopter:
+  def __init__(self):
+    self.x_cord = 0
+    self.y_cord = 0
+    self.image = pygame.image.load("helikopter.png")
+    
+  def tick(self):
+    pass
+  
+  def draw(self):
+    window.blit(self.image(self.x_cord,self.y_cord)      
+
+                
+class Samolot:
+  def __init__(self):
+    self.x_cord = 0
+    self.y_cord = 0
+    self.image = pygame.image.load("samolot.png")
+    
+  def tick(self):
+    pass
+  
+  def draw(self):
+    window.blit(self.image(self.x_cord,self.y_cord)
+                
+                
+class Paliwo:
+  def __init__(self):
+    self.x_cord = 0
+    self.y_cord = 0
+    self.image = pygame.image.load("paliwo.png")
+    
+  def tick(self):
+    pass
+  
+  def draw(self):
+    window.blit(self.image(self.x_cord,self.y_cord)
+                
+                
+class Most:
+  def __init__(self):
+    self.x_cord = 0
+    self.y_cord = 0
+    self.image = pygame.image.load("most.png")
+    
+  def tick(self):
+    pass
+  
+  def draw(self):
+    window.blit(self.image(self.x_cord,self.y_cord)
