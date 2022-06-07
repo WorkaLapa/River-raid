3843d6c26c4c17ccb50602c4c92735d7001a0182
WorkaLapa <105659697+WorkaLapa@users.noreply.github.com>
2022-06-07 14:42:02 +0200

    Update Start.py

--- Start.py
+++ Start.py
@@ -3,12 +3,11 @@ import random
 
 okno=pygame.display.set_mode(size=(900,600))
 clock=pygame.time.Clock()
-bok = random.randint(1,8)
-a=bok*50
+bok = 50*(random.randint(1,8))
 while True:
     for event in pygame.event.get():
-        pygame.draw.rect(okno, (0, 255, 0), (0, 0, a, 600))
-        pygame.draw.rect(okno, (0, 255, 0), (900-a, 0, 900, 600))
+        pygame.draw.rect(okno, (0, 255, 0), (0, 0, bok, 600))
+        pygame.draw.rect(okno, (0, 255, 0), (900-bok, 0, 900, 600))
         pygame.display.flip()
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_ESCAPE:
